da5fe3020e2e7de9bfaf12a77f3da3f416a90431
Aiman-48 <160266058+Aiman-48@users.noreply.github.com>
2024-04-04 21:20:05 -0700

    numb guessing game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// const randomNumber = Math.random();
+// console.log (randomNumber);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "guess a number between 1-10:"
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congrats");
+}
+else {
+    console.log("wrong numb");
+}
