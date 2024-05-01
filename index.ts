8c237929a975263e398327b1ea63df161efb0e25
Aiman-48 <160266058+Aiman-48@users.noreply.github.com>
2024-05-01 06:30:33 -0700

    FORMAT

--- index.ts
+++ index.ts
@@ -8,25 +8,16 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
-
-
-
 const answers = await inquirer.prompt([
-
-{
-    name : "userGuessedNumber",
-    type :"number",
-    message : "guess a number between 1-10:"
-
-}
-
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "guess a number between 1-10:",
+  },
 ]);
 
-if(answers.userGuessedNumber === randomNumber ){
-
-    console.log("congrats")
-
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("congrats");
 } else {
-
-    console.log("wrong numb")
+  console.log("wrong numb");
 }
